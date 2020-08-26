d8d9cdb8a3fdd30831b899b77096d99f49bb39d4
Daniel Fuentes <fuentes.danav@gmail.com>
2020-08-25 23:43:13 -0500

    Removing unnecessary files improving code readability

--- src/InitialInput.js
+++ src/InitialInput.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react"
+
 import { makeStyles } from "@material-ui/core/styles"
 import TextField from "@material-ui/core/TextField"
 import Container from "@material-ui/core/Container"
 import SearchIcon from "@material-ui/icons/Search"
 import Fab from "@material-ui/core/Fab"
 import Typography from "@material-ui/core/Typography"
-import FormGroup from "@material-ui/core/FormGroup"
 import FormControlLabel from "@material-ui/core/FormControlLabel"
 import Switch from "@material-ui/core/Switch"
 import axios from "axios"
@@ -29,10 +29,9 @@ import Badge from "@material-ui/core/Badge"
 import ButtonGroup from "@material-ui/core/ButtonGroup"
 import IconButton from "@material-ui/core/IconButton"
 import { ThemeProvider } from "@material-ui/styles"
-import { theme } from "./optionsStyles"
-import ImageLogo from "./ImageLogo"
+import { theme } from "./panels/optionsStyles"
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     "vertical-align": "baseline",
     "& > *": {
@@ -40,10 +39,9 @@ const useStyles = makeStyles((theme) => ({
       width: "25ch",
     },
   },
-
   counter: {
-    "display": "flex",
-    "flexDirection": "column",
+    display: "flex",
+    flexDirection: "column",
     "& > *": {
       marginBottom: theme.spacing(2),
     },
@@ -56,49 +54,124 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-const InitialInput = React.memo(() => {
-  const classes = useStyles()
-  const margin = { marginTop: "15px" }
-  const [tripOrigin, updateTripOrigin] = useState("Houston")
-  const [tripDestination, updateTripDestination] = useState("Orlando")
-  const [startingDate, setStartingDate] = useState(new Date("2020-07-6"))
-  const [endingDate, setEndingDate] = useState(startingDate)
-  const [peopleNumber, updatePeopleNumber] = useState(1)
+const mockResponse = {
+  data: {
+    initialTotal: 1259,
+    avgHotel: 161,
+    avgRestaurant: 23,
+    avgFlight: 339,
+    avgLH: 64,
+    avgMH: 103,
+    avgHH: 293,
+    avgLF: 153,
+    avgMF: 254,
+    avgHF: 610,
+    avgLR: 9,
+    avgMR: 19,
+    avgHR: 42,
+    hotelStars: [2, 3, 4, 5],
+    restaurantTypes: ["Burgers", "Pizza", "Mexican", "Japanese", "Steak"],
+    airlineNames: ["Spirit", "United", "American", "Delta", "Frontier"],
+  },
+}
 
-  const [customizationSwitchStates, updateCustomizationSwitchStates] = useState(
-    {
+const InitialInput = () => {
+  const classes = useStyles()
+  const [origin, updateOrigin] = useState("Houston")
+  const [destination, updateDestination] = useState("Orlando")
+  const [departure, setDeparture] = useState(new Date("2020-07-6"))
+  const [returnDate, setReturnDate] = useState(departure)
+  const [travelers, updateTravelers] = useState(1)
+
+  const [customOptions, updateCustomOptions] = useState({
+    flightTickets: true,
+    hotels: true,
+    restaurants: true,
+  })
+  const [response, updateResponse] = useState()
+  const [request, updateRequest] = useState()
+  const handleDepartureChange = date => {
+    setDeparture(date)
+  }
+
+  const handleReturnChange = date => {
+    setReturnDate(date)
+  }
+
+  const handleOriginChange = e => {
+    updateOrigin(e.target.value)
+  }
+  const handleDestinationChange = e => {
+    updateDestination(e.target.value)
+  }
+
+  const handleCustoms = event => {
+    updateCustomOptions({
+      ...customOptions,
+      [event.target.name]: event.target.checked,
+    })
+  }
+
+  const handleSearchClick = () => {
+    const currentRequest = {
+      origin: origin,
+      destination: destination,
+      days: parseInt(Math.abs(departure - returnDate) / (24 * 3600 * 1000)),
       flightTickets: true,
       hotels: true,
       restaurants: true,
+      people: travelers,
     }
-  )
-  const [responseData, updateResponseData] = useState(false)
-  const [requestData, updateRequestData] = useState(false)
-  const handleStartingDateChange = (date) => {
-    setStartingDate(date)
+
+    updateRequest(currentRequest)
+    sendInitialRequest(currentRequest)
   }
 
-  const handleEndingDateChange = (date) => {
-    setEndingDate(date)
+  const sendInitialRequest = async request => {
+    try {
+      let headers = new Headers()
+      headers.append("Content-Type", "application/json")
+      headers.append("Access-Control-Allow-Origin", "http://localhost:3000")
+      headers.append("Access-Control-Allow-Credentials", "true")
+
+      const requestBody = JSON.stringify(request)
+
+      const { data } =
+        mockResponse ||
+        (await axios({
+          url: "http://172.25.2.150:8080/first/",
+          method: "post",
+          data: requestBody,
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+        }))
+
+      await updateResponse({ ...data })
+    } catch (error) {
+      updateResponse(null)
+      console.error(error)
+    }
   }
 
   const Counter = () => {
     return (
       <>
-        <Badge color="primary" badgeContent={peopleNumber}>
+        <Badge color="primary" badgeContent={travelers}>
           <PeopleIcon />
         </Badge>
         <ButtonGroup>
           <IconButton
             onClick={() => {
-              updatePeopleNumber(Math.max(peopleNumber - 1, 1))
+              updateTravelers(Math.max(travelers - 1, 1))
             }}
           >
             <RemoveIcon fontSize="small" />
           </IconButton>
           <IconButton
             onClick={() => {
-              updatePeopleNumber(peopleNumber + 1)
+              updateTravelers(travelers + 1)
             }}
           >
             <AddIcon fontSize="small" />
@@ -108,148 +181,70 @@ const InitialInput = React.memo(() => {
     )
   }
 
-  const handleTripOriginChange = (e) => {
-    updateTripOrigin(e.target.value)
+  const CityInput = ({ cityType, handler }) => {
+    return (
+      <TextField
+        id="filled-basic"
+        label={cityType}
+        variant="filled"
+        onClick={event => event.stopPropagation()}
+        onFocus={event => event.stopPropagation()}
+        onChange={handler}
+      />
+    )
   }
-  const handleTripDestinationChange = (e) => {
-    updateTripDestination(e.target.value)
+
+  const DatePicker = ({ date, handler }) => {
+    return (
+      <KeyboardDatePicker
+        format="MM/dd/yyyy"
+        margin="normal"
+        id="first-day-picker"
+        label="Departure Day"
+        value={date}
+        onChange={handler}
+        onClick={event => event.stopPropagation()}
+        onFocus={event => event.stopPropagation()}
+        KeyboardButtonProps={{
+          "aria-label": "change date",
+        }}
+      />
+    )
   }
 
-  const handleSwitchChange = (event) => {
-    updateCustomizationSwitchStates({
-      ...customizationSwitchStates,
-      [event.target.name]: event.target.checked,
-    })
-  }
-
-  const handleSearchClick = () => {
-    sendInitialRequest()
-  }
-  const sendInitialRequest = async () => {
-    try {
-      let headers = new Headers()
-      headers.append("Content-Type", "application/json")
-      headers.append("Access-Control-Allow-Origin", "http://localhost:3000")
-      headers.append("Access-Control-Allow-Credentials", "true")
-
-      const requestData = {
-        origin: tripOrigin,
-        destination: tripDestination,
-        days: parseInt(
-          Math.abs(startingDate - endingDate) / (24 * 3600 * 1000)
-        ),
-        flightTickets: true,
-        hotels: true,
-        restaurants: true,
-        people: 2,
-      }
-
-      updateRequestData(requestData)
-
-      const requestBody = JSON.stringify(requestData)
-
-      const { data } = await axios({
-        url: "http://172.25.2.150:8080/first/",
-        method: "post",
-        data: requestBody,
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-        },
-      })
-
-      /* const data = {
-        initialTotal: 1259,
-        avgHotel: 161,
-        avgRestaurant: 23,
-        avgFlight: 339,
-        avgLH: 64,
-        avgMH: 103,
-        avgHH: 293,
-        avgLF: 153,
-        avgMF: 254,
-        avgHF: 610,
-        avgLR: 9,
-        avgMR: 19,
-        avgHR: 42,
-        hotelStars: [2, 3, 4, 5],
-        restaurantTypes: ["Burgers", "Pizza", "Mexican", "Japanese", "Steak"],
-        airlineNames: ["Spirit", "United", "American", "Delta", "Frontier"],
-      } */
-      updateResponseData(false)
-      updateResponseData({ ...data })
-    } catch (error) {
-      updateResponseData(false)
-      console.log(error)
-    }
+  const SearchButton = () => {
+    return (
+      <Fab variant="extended" onClick={handleSearchClick} color="primary">
+        <SearchIcon className={classes.extendedIcon} />
+        Search
+      </Fab>
+    )
   }
 
   return (
-    <div style={margin}>
+    <div style={{ marginTop: "15px" }}>
       <ThemeProvider theme={theme}>
-        <ImageLogo />
         <Container>
           <Card>
             <Grid container direction="column">
               <form className={classes.root} noValidate autoComplete="off">
-                <TextField
-                  id="filled-basic"
-                  label="Origin"
-                  variant="filled"
-                  onClick={(event) => event.stopPropagation()}
-                  onFocus={(event) => event.stopPropagation()}
-                  onChange={handleTripOriginChange}
-                />
-
-                <TextField
-                  id="filled-basic"
-                  label="Destination"
-                  variant="filled"
-                  onClick={(event) => event.stopPropagation()}
-                  onFocus={(event) => event.stopPropagation()}
-                  onChange={handleTripDestinationChange}
+                <CityInput cityType="Origin" handler={handleOriginChange} />
+                <CityInput
+                  cityType="Destination"
+                  handler={handleDestinationChange}
                 />
 
                 <MuiPickersUtilsProvider
                   style={{ verticalAlign: "baseline" }}
                   utils={DateFnsUtils}
                 >
-                  <KeyboardDatePicker
-                    format="MM/dd/yyyy"
-                    margin="normal"
-                    id="first-day-picker"
-                    label="Departure Day"
-                    value={startingDate}
-                    onChange={handleStartingDateChange}
-                    onClick={(event) => event.stopPropagation()}
-                    onFocus={(event) => event.stopPropagation()}
-                    KeyboardButtonProps={{
-                      "aria-label": "change date",
-                    }}
+                  <DatePicker
+                    date={departure}
+                    handler={handleDepartureChange}
                   />
-
-                  <KeyboardDatePicker
-                    format="MM/dd/yyyy"
-                    margin="normal"
-                    id="last-day-picker"
-                    label="Return Day"
-                    value={endingDate}
-                    onChange={handleEndingDateChange}
-                    onClick={(event) => event.stopPropagation()}
-                    onFocus={(event) => event.stopPropagation()}
-                    KeyboardButtonProps={{
-                      "aria-label": "change date",
-                    }}
-                  />
+                  <DatePicker date={returnDate} handler={handleReturnChange} />
                 </MuiPickersUtilsProvider>
-                <Fab
-                  variant="extended"
-                  onClick={handleSearchClick}
-                  color="primary"
-                >
-                  <SearchIcon className={classes.extendedIcon} />
-                  Search
-                </Fab>
+                <SearchButton />
               </form>
             </Grid>
           </Card>
@@ -258,70 +253,74 @@ const InitialInput = React.memo(() => {
               expandIcon={<ExpandMoreIcon />}
               aria-label="Expand"
               aria-controls="additional-actions1-content"
-              id="additional-actions1-header"
             >
-              <Typography gutterBottom align="center" variant="body1">
+              <Typography align="center" variant="body1">
                 Customize
               </Typography>
             </AccordionSummary>
             <AccordionDetails>
               <div className={classes.section2}>
-                <FormGroup row>
+                <Grid container row>
                   <Grid item>
                     <Counter />
                   </Grid>
-
-                  <FormControlLabel
-                    control={
-                      <Switch
-                        checked={customizationSwitchStates.flightTickets}
-                        onChange={handleSwitchChange}
-                        name="flightTickets"
-                        color="primary"
-                      />
-                    }
-                    label="Flight Tickets"
-                  />
-                  <FormControlLabel
-                    control={
-                      <Switch
-                        checked={customizationSwitchStates.hotels}
-                        onChange={handleSwitchChange}
-                        name="hotels"
-                        color="primary"
-                      />
-                    }
-                    label="Hotels"
-                  />
-                  <FormControlLabel
-                    control={
-                      <Switch
-                        checked={customizationSwitchStates.restaurants}
-                        onChange={handleSwitchChange}
-                        name="restaurants"
-                        color="primary"
-                      />
-                    }
-                    label="Restaurants"
-                  />
-                </FormGroup>
+                  <Grid item>
+                    <FormControlLabel
+                      control={
+                        <Switch
+                          checked={customOptions.flightTickets}
+                          onChange={handleCustoms}
+                          name="flightTickets"
+                          color="primary"
+                        />
+                      }
+                      label="Flight Tickets"
+                    />
+                  </Grid>
+                  <Grid item>
+                    <FormControlLabel
+                      control={
+                        <Switch
+                          checked={customOptions.hotels}
+                          onChange={handleCustoms}
+                          name="hotels"
+                          color="primary"
+                        />
+                      }
+                      label="Hotels"
+                    />
+                  </Grid>
+                  <Grid item>
+                    <FormControlLabel
+                      control={
+                        <Switch
+                          checked={customOptions.restaurants}
+                          onChange={handleCustoms}
+                          name="restaurants"
+                          color="primary"
+                        />
+                      }
+                      label="Restaurants"
+                    />
+                  </Grid>
+                </Grid>
               </div>
             </AccordionDetails>
           </Accordion>
         </Container>
         <Container>
-          {responseData && (
+          {response && (
             <InitialInputResult
-              requestData={requestData}
-              responseData={responseData}
-              peopleNumber={peopleNumber}
-              customizationSwitchStates={customizationSwitchStates}
+              requestData={request}
+              responseData={response}
+              peopleNumber={travelers}
+              customizationSwitchStates={customOptions}
             />
           )}
         </Container>
       </ThemeProvider>
     </div>
   )
-})
+}
 
 export default InitialInput
